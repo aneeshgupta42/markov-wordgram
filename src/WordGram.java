014953819b884e1195489ee9d09ef0fc7ef2f72d
Owen Astrachan <ola@cs.duke.edu>
2019-02-03 20:18:33 -0500

    initial commit

---
+++ src/WordGram.java
@@ -0,0 +1,84 @@
+
+/**
+ * A WordGram represents a sequence of strings
+ * just as a String represents a sequence of characters
+ * 
+ * @author YOUR NAME HERE
+ *
+ */
+public class WordGram {
+	
+	private String[] myWords;   
+	private String myToString;  // cached string
+	private int myHash;         // cached hash value
+
+	/**
+	 * Create WordGram (add comments)
+	 * @param source
+	 * @param start
+	 * @param size
+	 */
+	public WordGram(String[] source, int start, int size) {
+		myWords = new String[size];
+		myToString = "";
+		myHash = 5;
+		
+		// TODO: initialize all instance variables properly
+	}
+
+	/**
+	 * Return string at specific index in this WordGram
+	 * @param index in range [0..length() ) for string 
+	 * @return string at index
+	 */
+	public String wordAt(int index) {
+		if (index < 0 || index >= myWords.length) {
+			throw new IndexOutOfBoundsException("bad index in wordAt "+index);
+		}
+		return myWords[index];
+	}
+
+	/**
+	 * Complete this comment
+	 * @return
+	 */
+	public int length(){
+		// TODO: change this
+		return 0;
+	}
+
+
+	@Override
+	public boolean equals(Object o) {
+		if (! (o instanceof WordGram) || o == null){
+			return false;
+		}
+
+	    // TODO: complete this method
+		return true;
+	}
+
+	@Override
+	public int hashCode(){
+		// TODO: complete this method
+		return myHash;
+	}
+	
+
+	/**
+	 * Create and complete this comment
+	 * @param last is last String of returned WordGram
+	 * @return
+	 */
+	public WordGram shiftAdd(String last) {
+		WordGram wg = new WordGram(myWords,0,myWords.length);
+		// TODO: Complete this method
+		return wg;
+	}
+
+	@Override
+	public String toString(){
+		// TODO: Complete this method	
+		return myToString;
+	}
+}
